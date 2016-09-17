3f54cdaa319ad97a09bf112b78dfbfab666b9402
jegalsumin <jegalsumin@gmail.com>
2016-09-17 11:26:03 +0900

    Making the insertion sorting by binary search

--- Assignment/AL01/src/InsertionSort.java
+++ Assignment/AL01/src/InsertionSort.java
@@ -28,5 +28,56 @@ public class InsertionSort {
         return outputArray;
 
     }
+
+    public int[] binaryInsertionSorting(int[] inputArray){
+        int[] outputArray = new int[inputArray.length];
+        boolean outputArrayCheck;
+        outputArray[0] = inputArray[0];
+        
+        /*int[] outputArray = new int[inputArray.length];
+        boolean[] outputArrayCheck;
+        outputArray[0] = inputArray[0];
+        for(int inputArrayIndexforSorting=1; inputArrayIndexforSorting<inputArray.length; inputArrayIndexforSorting++){
+            outputArrayCheck = new boolean[inputArray.length];
+            int key = inputArray[inputArrayIndexforSorting];
+            int binarytimes = 2;
+            int outputArrayIndexforSorting = inputArrayIndexforSorting/binarytimes;
+            while(outputArrayIndexforSorting>=0 && outputArray[outputArrayIndexforSorting]!=key){
+                if(outputArrayCheck[outputArrayIndexforSorting]==true){
+                    break;
+                }
+                outputArrayCheck[outputArrayIndexforSorting] = true;
+                binarytimes *= 2;
+                if(key<outputArray[outputArrayIndexforSorting]) {
+                    outputArrayIndexforSorting =
+                            outputArrayIndexforSorting - (inputArrayIndexforSorting / binarytimes);
+                }
+                else {
+                    outputArrayIndexforSorting =
+                            outputArrayIndexforSorting + (inputArrayIndexforSorting / binarytimes);
+                }
+            }
+            //outputArrayIndexforSorting += 1;
+            //if(outputArrayIndexforSorting<0){
+            //    outputArrayIndexforSorting = 0;
+            //}
+            for(int outputIndexforCopy = inputArrayIndexforSorting; outputIndexforCopy>outputArrayIndexforSorting; outputIndexforCopy--){
+                outputArray[outputIndexforCopy] = outputArray[outputIndexforCopy-1];
+            }
+            outputArray[outputArrayIndexforSorting] = key;
+        }
+        return outputArray;*/
+
+    }
+
+    public static void main(String[] args){
+        InsertionSort i = new InsertionSort();
+        int[] input = {5, 2, 4, 6, 2, 3, 9, 8, 1, 7};
+        int[] expected = i.binaryInsertionSorting(input);
+        for(int k =0; k<expected.length; k++){
+            System.out.println(expected[k]);
+        }
+    }
     
 }
+
